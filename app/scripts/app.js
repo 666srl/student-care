948898d1c4009a0f697164ced294ab76e9fdefb5
srl <1244961548@qq.com>
2017-03-17 13:50:22 +0800

    11

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,37 @@
  * Main module of the application.
  */
 angular
-  .module('studentSystemApp', []);
+  .module('studentSystemApp', ['ui.router']).config(["$stateProvider","$urlRouterProvider",function($stateProvider,$urlRouterProvider){
+  	$stateProvider.state("login",{
+  		url:"/login",
+  		templateUrl:"views/login.html"
+  	})
+	
+	
+	.state("class",{
+  		url:"/class",
+  		templateUrl:"views/class.html"
+  	})
+  	
+  	.state("evaluate",{
+  		url:"/evaluate",
+  		templateUrl:"views/evaluate.html"
+  	})
+  	
+  	.state("punishment",{
+  		url:"/punishment",
+  		templateUrl:"views/punishment.html"
+  	})
+  	
+  	.state("studentdetcils",{
+  		url:"/studentdetcils",
+  		templateUrl:"views/studentdetcils.html"
+  	})
+  	
+  	.state("studentlist",{
+  		url:"/studentlist",
+  		templateUrl:"views/studentlist.html"
+  	})
+  	
+	$urlRouterProvider.otherwise("/login");
+  }]);;
